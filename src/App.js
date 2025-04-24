8215ee15d120231515c9076c9167910a9cdbc86a
knife <ashrafnaiefryan@gmail.com>
2025-04-24 20:45:47 +0530

    Added budget graph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Todo from "./ToDo";
 import Deadlines from "./Deadlines";
+import BudgetChart from "./Budget";
 
 function App() {
   return (
     <div className="App">
       <Todo />
       <Deadlines title="idk" dueDate="2025-04-27T23:59:00" progress="80" />
+      <BudgetChart />
     </div>
   );
 }
---
+++ src/Budget.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import { valueOrDefault } from "chart.js/helpers";
+
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
+
+const BudgetChart = () => {
+  const labels = ["Food", "Clothes", "Transport", "Health", "Misc"];
+  const dataValues = [500, 1000, 300, 400, 69];
+  const colors = ["#4F46E5", "#10B981", "#F59E0B", "#EF4444", "#8B5CF6"];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Spent",
+        data: dataValues,
+        borderWidth: 1,
+        backgroundColor: colors,
+        hoverOffset: 20,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: false,
+      },
+      datalabels: {
+        color: "white",
+        formatter: (value, context) => {
+          return context.chart.data.labels[context.dataIndex];
+        },
+        font: {
+          weight: "bold",
+        },
+      },
+    },
+  };
+
+  return (
+    <div
+      className="container budget"
+      style={{ width: "400px", margin: "0 auto" }}
+    >
+      <h2>
+        Total Spent:{" ₹"}
+        {dataValues.reduce((v, s) => {
+          return v + s;
+        })}
+      </h2>
+      <Doughnut className="chart" data={data} options={chartOptions} />
+    </div>
+  );
+};
+
+export default BudgetChart;
